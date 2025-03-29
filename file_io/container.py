7fbdf5ad677d291a25598760a5bf27168d5ddf7f
julian-icruz <julian.iguavita@outlook.com>
2025-03-29 14:49:23 -0500

    feat: Enhance FileIOContainer with multiple reader factories

--- file_io/container.py
+++ file_io/container.py
@@ -1,5 +1,34 @@
+from dependency_injector.providers import Dict, Factory
 from dependency_injector.containers import DeclarativeContainer
+
+from file_io.infrastructure.adapters import (
+    PandasCSVReader,
+    PandasJSONReader,
+    PandasParquetReader,
+    PolarsCSVReader,
+    PolarsParquetReader,
+    DaskCSVReader,
+    DaskParquetReader,
+    JSONReader,
+    YAMLReader,
+)
 
 
 class FileIOContainer(DeclarativeContainer):
-    pass
+    readers = Dict(
+        csv=Dict(
+            pandas=Factory(PandasCSVReader),
+            polars=Factory(PolarsCSVReader),
+            dask=Factory(DaskCSVReader),
+        ),
+        json=Dict(
+            pandas=Factory(PandasJSONReader),
+            structured=Factory(JSONReader),
+        ),
+        parquet=Dict(
+            pandas=Factory(PandasParquetReader),
+            polars=Factory(PolarsParquetReader),
+            dask=Factory(DaskParquetReader),
+        ),
+        yaml=Factory(YAMLReader),
+    )
