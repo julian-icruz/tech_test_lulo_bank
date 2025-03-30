a8ce4484a2da2d384d881e0bdd31224242819333
julian-icruz <julian.iguavita@outlook.com>
2025-03-30 18:23:57 -0500

    feat: Add ProfilingService for orchestrating data profiling processes

---
+++ app/transform/application/services/__init__.py
@@ -0,0 +1,5 @@
+from app.transform.application.services.profiling_service import ProfilingService
+
+__ALL__ = [
+    ProfilingService,
+]
---
+++ app/transform/application/services/profiling_service.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from typing import Any, Dict
+from transform.domain.ports.data_profiling_port import DataProfilingPort
+
+
+@dataclass
+class ProfilingService:
+    profiling_adapter: DataProfilingPort
+
+    def profile(self, data: Any) -> Dict[str, Any]:
+        """
+        Orchestrates the data profiling process by invoking methods from the data profiling adapter.
+
+        Args:
+            data (Any): Input data (e.g., a DataFrame from pandas, polars, or dask).
+
+        Returns:
+            Dict[str, Any]: A dictionary containing profiling results including descriptive statistics,
+                            missing values, duplicates, correlations, and column type profiles.
+        """
+        results = {}
+        results["descriptive_statistics"] = (
+            self.profiling_adapter.generate_descriptive_statistics(data)
+        )
+        results["missing_values"] = self.profiling_adapter.detect_missing_values(data)
+        results["duplicates"] = self.profiling_adapter.detect_duplicates(data)
+        results["correlations"] = self.profiling_adapter.compute_correlations(data)
+        results["column_types"] = self.profiling_adapter.profile_column_types(data)
+        return results
