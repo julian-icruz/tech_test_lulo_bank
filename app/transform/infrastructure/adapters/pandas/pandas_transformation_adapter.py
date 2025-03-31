cc1b3d90b2177b85f4dd1a6c37481383023fa63e
julian-icruz <julian.iguavita@outlook.com>
2025-03-30 22:10:08 -0500

    feat: Add transformation adapters for Pandas, Polars, and Dask to support data processing

---
+++ app/transform/infrastructure/adapters/pandas/pandas_transformation_adapter.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from typing import Any, List
+from dataclasses import dataclass
+
+from app.transform.domain.ports.data_transformation_port import DataTransformationPort
+
+
+@dataclass
+class PandasTransformation(DataTransformationPort):
+    def flatten_nested_structures(self, data: Any) -> Any:
+        pass
+
+    def convert_date_time_columns(self, data: Any) -> Any:
+        pass
+
+    def normalize_columns(self, data: Any) -> Any:
+        pass
+
+    def categorize_columns(self, data: Any) -> Any:
+        pass
+
+    def merge_dataframes(self, data_list: List[Any]) -> Any:
+        """
+        Merges a list of pandas DataFrames into a single DataFrame.
+
+        Args:
+            data_list (List[Any]): A list of pandas DataFrame objects.
+
+        Returns:
+            Any: A single pandas DataFrame resulting from concatenation.
+        """
+        if not data_list:
+            raise ValueError("No data provided for merging.")
+        return pd.concat(data_list, ignore_index=True)
