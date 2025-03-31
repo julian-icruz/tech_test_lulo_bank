d88e1043293aaed231b62a8532ff440f2f415384
julian-icruz <julian.iguavita@outlook.com>
2025-03-30 20:04:20 -0500

    feat: Enhance TransformContainer with Singleton services for profiling and report generation

--- app/transform/container.py
+++ app/transform/container.py
@@ -1,7 +1,25 @@
-from dependency_injector.providers import Dependency
-from dependency_injector.containers import DeclarativeContainer
+from dependency_injector.providers import Dependency, Singleton
+from dependency_injector.containers import DeclarativeContainer, WiringConfiguration
+
+from app.transform.infrastructure.adapters import ReportGeneratorAdapter
+from app.transform.application.services import (
+    ProfilingService,
+    ProfilingReportService,
+)
 
 
 class TransformContainer(DeclarativeContainer):
+    wiring_config = WiringConfiguration(
+        packages=[
+            "app.transform.routes",
+        ]
+    )
 
     file_io = Dependency()
+
+    report_generator_adapter = Singleton(ReportGeneratorAdapter)
+
+    profiling_service = Singleton(
+        ProfilingService,
+        report_generator_adapter=report_generator_adapter,
+    )
