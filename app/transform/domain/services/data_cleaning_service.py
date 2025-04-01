6f1b5abd147df9adb546cf5562036fc63d72ad0a
julian-icruz <julian.iguavita@outlook.com>
2025-03-31 19:24:07 -0500

    feat: Refactor DataCleaningService and related components for improved data processing and file writing

--- app/transform/domain/services/data_cleaning_service.py
+++ app/transform/domain/services/data_cleaning_service.py
@@ -7,45 +7,44 @@ class DataCleaningService(BaseTransformService):
 
     def __call__(self, data: Any) -> dict[str, Any]:
         """
-        Orchestrates the data profiling process by invoking methods from the data profiling adapter.
-
+        Orchestrates the data cleaning process:
+            1. Flattens nested structures in the data.
+            2. Filters and cleans empty columns.
+            3. Merges DataFrames.
+            4. Processes shows and episodes: converts lists to strings and removes duplicates.
+            5. Extracts DataFrames for web_channel and networks.
+            6. Renames columns in all DataFrames.
+            7. Converts date/time columns.
+            8. Replaces NaN values with None.
+            9. Normalizes numeric columns.
+            10. Evaluates literal columns.
+            11. Replaces empty strings with None.
         Args:
-            data (Any): Input data (e.g., a DataFrame from pandas, polars, or dask).
-
+            data (Any): List of DataFrames to be processed.
         Returns:
-            Dict[str, Any]: A dictionary containing profiling results including descriptive statistics,
-                            missing values, duplicates, correlations, and column type profiles.
+            dict[str, Any]: A dictionary containing processed DataFrames for shows, episodes, web_channel, and networks.
         """
-        data_flattened_show = []
-        data_flattened_episodes = []
+        flattened_shows = [self.transformation_adapter.flatten_show(df) for df in data]
+        flattened_episodes = [
+            self.transformation_adapter.flatten_episodes(df) for df in data
+        ]
 
-        for df in data:
-            data_flattened_show.append(self.transformation_adapter.flatten_show(df))
-            data_flattened_episodes.append(
-                self.transformation_adapter.flatten_episodes(df)
-            )
-
-        list_df_show_filtered = (
-            self.transformation_adapter.filter_and_clean_empty_columns(
-                data_flattened_show
-            )
+        filtered_shows = self.transformation_adapter.filter_and_clean_empty_columns(
+            flattened_shows
         )
 
-        data_show_merged = self.transformation_adapter.merge_dataframes(
-            list_df_show_filtered
-        )
-        data_episodes_merged = self.transformation_adapter.merge_dataframes(
-            data_flattened_episodes
+        merged_shows = self.transformation_adapter.merge_dataframes(filtered_shows)
+        merged_episodes = self.transformation_adapter.merge_dataframes(
+            flattened_episodes
         )
 
         df_shows = (
             self.transformation_adapter.convert_lists_to_strings_and_drop_duplicates(
-                data_show_merged
+                merged_shows
             )
         )
-
         df_episodes = self.transformation_adapter.clean_episode_duplicates(
-            data_episodes_merged
+            merged_episodes
         )
 
         df_shows, df_web_channel, df_networks = (
@@ -54,6 +53,42 @@ class DataCleaningService(BaseTransformService):
 
         df_shows = self.transformation_adapter.rename_columns(df_shows)
         df_web_channel = self.transformation_adapter.rename_columns(df_web_channel)
-        df_networks = self.transformation_adapter.rename_columns(df_web_channel)
+        df_networks = self.transformation_adapter.rename_columns(df_networks)
+
+        df_shows = self.transformation_adapter.convert_date_time_columns(
+            df_shows, "updated"
+        )
+
+        df_shows = self.transformation_adapter.replace_nan_with_none(df_shows)
+        df_episodes = self.transformation_adapter.replace_nan_with_none(df_episodes)
+        df_web_channel = self.transformation_adapter.replace_nan_with_none(
+            df_web_channel
+        )
+        df_networks = self.transformation_adapter.replace_nan_with_none(df_networks)
+
+        columns_show = [
+            "id",
+            "averageruntime",
+            "weight",
+            "thetvdb",
+            "runtime",
+            "network_id",
+            "webchannel_id",
+            "tvrage",
+        ]
+        df_shows = self.transformation_adapter.normalize_columns(df_shows, columns_show)
+
+        columns_episodes = ["id", "season", "number", "runtime", "show_id"]
+        df_episodes = self.transformation_adapter.normalize_columns(
+            df_episodes, columns_episodes
+        )
+
+        df_shows = self.transformation_adapter.evaluate_literal_column(
+            df_shows, "genres"
+        )
+
+        df_episodes = self.transformation_adapter.replace_empty_with_none(
+            df_episodes, "airtime"
+        )
 
         return df_shows, df_episodes, df_web_channel, df_networks
